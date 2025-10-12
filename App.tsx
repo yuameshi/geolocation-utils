8d5ab448f3f23f2c3d19fb54d30a01ca281466bc
糸 <64469437+yuameshi@users.noreply.github.com>
2025-10-12 14:31:37 +0800

    chore(style): edit prettier config & format code

--- App.tsx
+++ App.tsx
@@ -7,39 +7,36 @@
 
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+	const isDarkMode = useColorScheme() === 'dark';
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+	return (
+		<SafeAreaProvider>
+			<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+			<AppContent />
+		</SafeAreaProvider>
+	);
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+	const safeAreaInsets = useSafeAreaInsets();
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<NewAppScreen
+				templateFileName="App.tsx"
+				safeAreaInsets={safeAreaInsets}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+	container: {
+		flex: 1,
+	},
 });
 
 export default App;
