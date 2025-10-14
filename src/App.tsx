bda061bb28d385192c1ecd80bf2482bbf3197e15
糸 <64469437+yuameshi@users.noreply.github.com>
2025-10-14 23:51:20 +0800

    feat(App): implemented basic router ( page replacement )

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,30 @@ import { PaperProvider, MD3DarkTheme, MD3LightTheme } from 'react-native-paper';
 import { StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Odometer } from './pages/Odometer';
+import { createContext, createElement, useState } from 'react';
+
+export const RouterContext = createContext<{
+	route: string;
+	setRoute: (route: string) => void;
+	routes: { [key: string]: React.ComponentType<any> };
+} | null>(null);
 
 function App() {
 	const isDarkMode = useColorScheme() === 'dark';
 	const theme = isDarkMode ? MD3DarkTheme : MD3LightTheme;
+	const [route, setRoute] = useState('Odometer');
+
+	const routes: { [key: string]: React.ComponentType<any> } = {
+		Odometer: Odometer,
+	};
 
 	return (
 		<PaperProvider theme={theme}>
 			<SafeAreaProvider>
 				<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-				<Odometer />
+				<RouterContext.Provider value={{ route, setRoute, routes }}>
+					<>{route && routes[route] ? createElement(routes[route]) : <Odometer />}</>
+				</RouterContext.Provider>
 			</SafeAreaProvider>
 		</PaperProvider>
 	);
