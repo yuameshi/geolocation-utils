8b0eebd1880680b3250b7c6bda7cded6520294ed
糸 <64469437+yuameshi@users.noreply.github.com>
2025-11-10 00:43:22 +0800

    feat: implement tile AvgSpeed ( untested )

--- src/pages/Odometer/components/Tiles/AvgSpeed.tsx
+++ src/pages/Odometer/components/Tiles/AvgSpeed.tsx
@@ -1,17 +1,45 @@
-import type { FC } from 'react';
+import Geolocation from '@react-native-community/geolocation';
+import { useEffect, useRef, useState, type FC } from 'react';
 import { Surface, Icon, Text } from 'react-native-paper';
 import { UnitAdapter } from '@utils/unit-adapter';
 import { styles } from './styles';
 import { useVerticalLayout } from '@hooks/useVerticalLayout';
 import { useStoredValue } from '@hooks/useStoredState';
 
-type Props = {
-	accuracy: number | null;
-};
-
-export const AvgSpeed: FC<Props> = ({ accuracy }) => {
+export const AvgSpeed: FC = () => {
 	const verticalLayout = useVerticalLayout();
+	const startTs = (parseInt(useStoredValue('session.appStartedAt') || '0', 10) ?? 0) / 1000;
 	const unit = useStoredValue<'Metric' | 'Imperial'>('settings.unit') ?? 'Metric';
+	const [avgSpeed, setAvgSpeed] = useState(0);
+	// use this to store last speed when speed is null
+	const lastSpeed = useRef(0);
+
+	useEffect(() => {
+		const watchId = Geolocation.watchPosition(
+			geolocation => {
+				const nowTs = geolocation.timestamp / 1000;
+				const timeDiff = nowTs - startTs;
+				if (timeDiff > 0) {
+					const prevTotalSpeed = avgSpeed * (timeDiff - 1);
+					const newTotalSpeed = prevTotalSpeed + (geolocation.coords.speed || lastSpeed.current);
+					const newAvgSpeed = newTotalSpeed / timeDiff;
+					setAvgSpeed(newAvgSpeed);
+					lastSpeed.current = geolocation.coords.speed ?? lastSpeed.current;
+				}
+			},
+			error => console.warn('Error getting FINE location at AvgSpeed Module: ', error),
+			{
+				enableHighAccuracy: true,
+				distanceFilter: 0,
+				interval: 1000,
+				fastestInterval: 1000,
+			},
+		);
+		return () => {
+			Geolocation.clearWatch(watchId);
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<Surface
@@ -23,7 +51,7 @@ export const AvgSpeed: FC<Props> = ({ accuracy }) => {
 				size={24}
 			/>
 			<Text style={styles.label}>Avg. Speed</Text>
-			<Text style={styles.value}>{accuracy === null ? '-.-' : UnitAdapter[unit].accuracy(accuracy).toFixed(1)}</Text>
+			<Text style={styles.value}>{avgSpeed === null ? '-.-' : UnitAdapter[unit].speed(avgSpeed).toFixed(1)}</Text>
 			<Text style={styles.hint}>{UnitAdapter[unit].units.speed}</Text>
 		</Surface>
 	);
