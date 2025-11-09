c8cf5c6cbd48d765c5428acd50554b619f5a1cb3
糸 <64469437+yuameshi@users.noreply.github.com>
2025-11-09 18:06:51 +0800

    feat(android): will keep screen awake after app started

--- android/app/src/main/java/top/yuameshi/geolocation/utils/MainActivity.kt
+++ android/app/src/main/java/top/yuameshi/geolocation/utils/MainActivity.kt
@@ -1,11 +1,17 @@
 package top.yuameshi.geolocation.utils
 
+import android.os.Bundle
+import android.view.WindowManager
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    window.addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
